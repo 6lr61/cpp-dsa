d6b552fef7e660e74e7f4b42788107943f40ffb0
Adrian <107351903+6lr61@users.noreply.github.com>
2024-05-10 12:56:33 +0200

    feat: depth first search

--- src/binary_tree.hpp
+++ src/binary_tree.hpp
@@ -5,6 +5,9 @@ template <typename T> struct Node {
   Node *left{};
   Node *right{};
   Node *parent{};
+  T value;
+
+  Node(Node *p, T v) : parent(p), value(v) {}
 };
 
 template <typename T> std::size_t size(Node<T> *node) {
@@ -21,4 +24,41 @@ template <typename T> int height(Node<T> *node) {
   }
 
   return 1 + std::max(height(node->left), height(node->right));
+}
+
+bool search(auto *node, auto function) {
+  decltype(node) curr = node;
+  decltype(node) prev = nullptr;
+  decltype(node) next;
+
+  while (curr != nullptr) {
+    if (prev == curr->parent) {
+      bool match = function(curr);
+
+      if (match) {
+        return match;
+      }
+
+      if (curr->left != nullptr) {
+        next = curr->left;
+      } else if (curr->right != nullptr) {
+        next = curr->right;
+      } else {
+        next = curr->parent;
+      }
+    } else if (prev == curr->left) {
+      if (curr->right != nullptr) {
+        next = curr->right;
+      } else {
+        next = curr->parent;
+      }
+    } else {
+      next = curr->parent;
+    }
+
+    prev = curr;
+    curr = next;
+  }
+
+  return false;
 }
--- test/binary_tree.cpp
+++ test/binary_tree.cpp
@@ -1,12 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+#include <cstddef>
 
 #include "../src/binary_tree.hpp"
 
 TEST_CASE("Intialize a tree of size 3 and height 2") {
-  Node<int> root;
-  Node<int> left;
-  Node<int> right;
-  Node<int> left_left;
+  Node<int> root{nullptr, 1};
+  Node<int> left{&root, 2};
+  Node<int> right{&root, 3};
+  Node<int> left_left{&left, 4};
 
   root.left = &left;
   root.right = &right;
@@ -15,4 +16,54 @@ TEST_CASE("Intialize a tree of size 3 and height 2") {
 
   REQUIRE(size(&root) == 4);
   REQUIRE(height(&root) == 2);
+}
+
+TEST_CASE("Traverse and count number nodes") {
+  Node<int> root{nullptr, 1};
+  Node<int> left{&root, 2};
+  Node<int> right{&root, 3};
+  Node<int> left_left{&left, 4};
+
+  root.left = &left;
+  root.right = &right;
+
+  left.left = &left_left;
+
+  std::size_t n_nodes = 0;
+  auto count = [&n_nodes](auto *) {
+    n_nodes++;
+    return false;
+  };
+
+  search(&root, count);
+
+  REQUIRE(size(&root) == n_nodes);
+}
+
+TEST_CASE("Traverse and sum all values") {
+  Node<int> root{nullptr, 1};
+  Node<int> left{&root, 2};
+  Node<int> right{&root, 3};
+  root.left = &left;
+  root.right = &right;
+
+  int sum = 0;
+  auto function = [&sum](auto *node) {
+    sum += node->value;
+    return false;
+  };
+
+  search(&root, function);
+
+  REQUIRE(sum == 6);
+}
+
+TEST_CASE("Find a specific node") {
+  Node<int> root{nullptr, 1};
+  Node<int> left{&root, 2};
+  Node<int> right{&root, 3};
+  root.left = &left;
+  root.right = &right;
+
+  REQUIRE(search(&root, [](auto *node) { return node->value == 3; }));
 }
