68c8b41a35f440bfd3a2bdadc4dc875a9561edc6
Adrian <107351903+6lr61@users.noreply.github.com>
2024-05-10 14:35:29 +0200

    feat: start of binary search tree

--- src/binary_tree.hpp
+++ src/binary_tree.hpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <cstddef>
+#include <iostream>
+#include <string>
 
 template <typename T> struct Node {
   Node *left{};
@@ -8,6 +10,10 @@ template <typename T> struct Node {
   T value;
 
   Node(Node *p, T v) : parent(p), value(v) {}
+
+  auto operator<=>(const Node &node) const { return value <=> node.value; }
+
+  auto operator<=>(const T v) const { return value <=> v; }
 };
 
 template <typename T> std::size_t size(Node<T> *node) {
@@ -61,4 +67,53 @@ bool search(auto *node, auto function) {
   }
 
   return false;
+}
+
+template <typename T> Node<T> *find_last(Node<T> *root, T value) {
+  Node<T> *curr = root;
+  Node<T> *prev = nullptr;
+
+  while (curr != nullptr) {
+    prev = curr;
+    auto comp = *curr <=> value;
+
+    if (comp > 0) {
+      std::cout << curr->value << " greater than " << value << '\n';
+      curr = curr->left;
+    } else if (comp < 0) {
+      std::cout << curr->value << " less than " << value << '\n';
+      curr = curr->right;
+    } else {
+      std::cout << curr->value << " equal to " << value << '\n';
+      return curr;
+    }
+  }
+
+  return prev;
+}
+
+template <typename T> bool add_child(Node<T> *parent, Node<T> *child) {
+  if (parent == nullptr || child == nullptr) {
+    return false;
+  }
+
+  auto comp = *parent <=> *child;
+
+  if (comp < 0) {
+    parent->right = child;
+  } else if (comp > 0) {
+    parent->left = child;
+  } else {
+    return false;
+  }
+
+  child->parent = parent;
+
+  return true;
+}
+
+template <typename T> bool add(Node<T> *root, T value) {
+  Node<T> *parent = find_last(root, value);
+  // FIXME: This is a memory leak!
+  return add_child(parent, new Node<T>{nullptr, value});
 }
--- test/binary_tree.cpp
+++ test/binary_tree.cpp
@@ -67,3 +67,55 @@ TEST_CASE("Find a specific node") {
 
   REQUIRE(search(&root, [](auto *node) { return node->value == 3; }));
 }
+
+TEST_CASE("Find the last node") {
+  Node<int> root{nullptr, 5};
+  Node<int> left{&root, 3};
+  Node<int> right{&root, 7};
+  root.left = &left;
+  root.right = &right;
+
+  REQUIRE(find_last(&root, 2)->value == 3);
+  REQUIRE(find_last(&root, 8)->value == 7);
+  REQUIRE(find_last(&root, 3)->value == 3);
+}
+
+TEST_CASE("Add smaller child to left branch") {
+  Node<int> parent{nullptr, 3};
+  Node<int> child{nullptr, 1};
+
+  REQUIRE(add_child(&parent, &child));
+  REQUIRE(child.parent == &parent);
+  REQUIRE(parent.left == &child);
+}
+
+TEST_CASE("Add bigger child to right branch") {
+  Node<int> parent{nullptr, 3};
+  Node<int> child{nullptr, 5};
+
+  REQUIRE(add_child(&parent, &child));
+  REQUIRE(child.parent == &parent);
+  REQUIRE(parent.right == &child);
+}
+
+TEST_CASE("Don't add equal child to any branch") {
+  Node<int> parent{nullptr, 3};
+  Node<int> child{nullptr, 3};
+
+  REQUIRE(!add_child(&parent, &child));
+  REQUIRE(child.parent == nullptr);
+  REQUIRE(parent.left == nullptr);
+  REQUIRE(parent.right == nullptr);
+}
+
+TEST_CASE("Build a binary tree") {
+  Node<int> parent{nullptr, 5};
+
+  REQUIRE(add<int>(&parent, 3));
+  REQUIRE(add<int>(&parent, 7));
+  REQUIRE(add<int>(&parent, 2));
+
+  REQUIRE(parent.left->value == 3);
+  REQUIRE(parent.left->left->value == 2);
+  REQUIRE(parent.right->value == 7);
+}
